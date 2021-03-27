c5b4e24b4da6de5cb15cd35ffc480ec7d763482b
anthonycoded <barbels33@gmail.com>
2021-03-26 20:17:03 -0400

    new section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,21 +3,78 @@ import avatar from "../assets/avatar.jpg";
 
 const LandingPage = () => {
   return (
-    <div className=" flex flex-col justify-center p-8 items-center">
-      <p className="font-bold text-4xl text-black mb-4 text-center p-8 mt-4 capitalize">
-        Hello World, my name is <br></br>{" "}
-        <span className="text-material-blue">Shawn</span> barbel
-      </p>
-      <div className=" flex flex-col items-center">
-        <img src={avatar} alt="" className="rounded-full h-48 mb-8"></img>
-        <div className="flex flex-col justify-center items-center">
+    <div className="h-full flex flex-col justify-center items-center space-y-4">
+      <div className="h-full flex justify-center p-4 pt-8">
+        <p className="font-bold text-4xl text-black text-center mt-14 capitalize">
+          Hello World, <br></br> my name is{" "}
+          <span className="text-material-blue">Shawn</span> barbel
+        </p>
+      </div>
+      <div className="h-full flex flex-col items-center space-y-4 p-4">
+        <img src={avatar} alt="" className="rounded-full h-48 mb-4"></img>
+        <div className="h-full flex flex-col justify-center items-center ">
           <p className="text-2xl font-bold text-material-purple">
             Software Developer
           </p>
-          <p className="text-xl text-gray-500 font-medium mb-4">Solution Creator</p>
-          <p className="mb-2">Need help with your website?</p>
-          <button className="rounded bg-material-blue w-20">Hire Me</button>
+          <p className="text-xl text-gray-500 font-medium mb-4">
+            Solution Creator
+          </p>
         </div>
+        <div className="h-full flex flex-col items-center p-4">
+          <p className="mb-2">Need help with your website?</p>
+          <button className="h-12 rounded-xl font-medium bg-material-blue w-28 mb-8">
+            Hire Me
+          </button>
+        </div>
+      </div>
+      <div className="h-full w-full flex flex-col bg-blue-400 p-4 space-y-16">
+        <p className="text-2xl text-black font-bold text-left mb-4">
+          What I can Do
+        </p>
+        <div className="w-full h-full flex justify-between">
+          <div className="rounded-xl bg-yellow-400 h-20 w-36 justify-center items-center p-4">
+            <p className="text-center font-medium">E-Commerce</p>
+          </div>
+
+          <p className="text-center font-medium w-60">
+            Need to sell products online?Need to sell products online?Need to
+            sell products online?Need
+          </p>
+        </div>
+
+        <div className="h-full w-full flex justify-between">
+          <div className="rounded-xl bg-red-400 h-20 w-36 flex justify-center items-center p-4">
+            <p className="text-center font-medium">Content Management</p>
+          </div>
+
+          <p className="text-center font-medium w-60">
+            Need to sell products online?Need to sell products online?Need to
+            sell products online?Need to sell products on
+          </p>
+        </div>
+        <div className="w-full h-full flex justify-between">
+          <div className="rounded-xl bg-green-400 h-20 w-36 flex justify-center items-center p-4">
+            <p className="text-center font-medium">CRM</p>
+          </div>
+
+          <p className="text-center font-medium w-60">
+            Need to sell products online?Need to sell products online?Need to
+            sell products online?Need to sell products on
+          </p>
+        </div>
+        <div className="w-full h-full flex justify-between mb-12">
+          <div className="rounded-xl bg-pink-400 h-20 w-36 mb-4  justify-center items-center p-4">
+            <p className="text-center font-medium">Music Streaming</p>
+          </div>
+
+          <p className="text-center font-medium w-60 mb-12">
+            Need to sell products online?Need to sell products online?Need to
+            sell products online?Need to sell products on
+          </p>
+        </div>
+      </div>
+      <div className="h-full w-full flex flex-col p-4">
+        <p className="text-2xl text-black font-bold text-left">Projects</p>
       </div>
     </div>
   );
