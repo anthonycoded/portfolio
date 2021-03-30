71c37e35cdb7d01c6c12ea2ad4d8014b0af16cc5
anthonycoded <barbels33@gmail.com>
2021-03-30 17:07:28 -0400

    added projects

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,14 @@
 import React from "react";
+
 import avatar from "../assets/avatar.jpg";
+import adminwishlist from "../assets/adminwishlist.png";
+import covidtracker from "../assets/covidtracker.png";
+import map from "../assets/map.png";
+import bdmediaplayer from "../assets/bdmediaplayer.png";
+import bdlandingpage from "../assets/bdlandingpage.png";
+import bdlogin from "../assets/bdlogin.png";
+import newproduct from "../assets/newproduct.png";
+import numbers from "../assets/numbers.png";
 
 const LandingPage = () => {
   return (
@@ -73,8 +82,62 @@ const LandingPage = () => {
           </p>
         </div>
       </div>
-      <div className="h-full w-full flex flex-col p-4">
-        <p className="text-2xl text-black font-bold text-left">Projects</p>
+      <div className="h-full w-full flex flex-col p-4 space-y-12">
+        <p className="text-3xl text-black font-bold text-left mb-12">
+          Projects
+        </p>
+        <div className="h-full p-4 space-y-4">
+          <div>
+            <p className="text-2xl font-bold text-yellow-400">BeatDealers</p>
+            <p className="">
+              The goal of this project is to lease and sell beats to artist. It
+              includes audio streaming, authentication, payment processing, file
+              uploading and downloading.
+            </p>
+          </div>
+          <p className="text-md font-medium ">Screenshots:</p>
+          <div className="flex overflow-x-scroll w-full space-x-8 p-4">
+            <img src={bdlandingpage} alt="" className="w-60"></img>
+            <img src={bdmediaplayer} alt="" className="w-60"></img>
+            <img src={bdlogin} alt="" className="w-60"></img>
+          </div>
+          <p className="text-xl font-medium">
+            Technologies used: MERN Stack, JWT, Google Cloud Platform, Stripe
+          </p>
+        </div>
+
+        <div className="h-full">
+          <p className="text-2xl font-bold text-blue-400">Wishlist</p>
+          <div className="flex overflow-x-scroll w-full space-x-8 p-4">
+            <img src={adminwishlist} alt="" className="w-60"></img>
+            <img src={newproduct} alt="" className="w-60"></img>
+          </div>
+          <p className="p-4 text-xl font-medium">
+            Technologies used: MERN Stack, Google Cloud Platform, Stripe
+          </p>
+          <p className="p-4">
+            The goal of this project is to lease and sell beats to artist. It
+            includes audio streaming, authentication, payment processing, file
+            uploading and downloading.
+          </p>
+        </div>
+
+        <div className="h-full">
+          <p className="text-2xl font-bold text-red-400">Covid Tracker</p>
+          <div className="flex overflow-x-scroll w-full space-x-8 p-4">
+            <img src={covidtracker} alt="" className="w-60"></img>
+            <img src={numbers} alt="" className="w-60"></img>
+            <img src={map} alt="" className="w-60"></img>
+          </div>
+          <p className="p-4 text-xl font-medium">
+            Technologies used: MERN Stack, Google Cloud Platform, Stripe
+          </p>
+          <p className="p-4">
+            The goal of this project is to lease and sell beats to artist. It
+            includes audio streaming, authentication, payment processing, file
+            uploading and downloading.
+          </p>
+        </div>
       </div>
     </div>
   );
