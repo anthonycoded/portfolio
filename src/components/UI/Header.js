d233c07189aa9d9f539bb7827c2a6dd04dcdaa02
anthonycoded <barbels33@gmail.com>
2021-04-02 19:43:11 -0400

    added hemburger menu to header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import menu from "../../assets/menu.svg";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -36,11 +37,15 @@ const Header = () => {
       <div className="h-12 flex justify-between items-center bg-material-purple w-full px-2 fixed">
         <a href="/">
           <h1 className="pl-4 text-material-blue font-bold text-2xl">
-            {open ? undefined : <p className="text-black"><span className="text-material-blue">Shawn</span>Barbel</p>}
+            {open ? undefined : (
+              <p className="text-black">
+                <span className="text-material-blue">Shawn</span>Barbel
+              </p>
+            )}
           </h1>
         </a>
         <button className="" onClick={() => setOpen(!open)}>
-          menu
+          <img src={menu} alt="menu" className="h-9"></img>
         </button>
       </div>
       {open ? <OpenHeader></OpenHeader> : undefined}
