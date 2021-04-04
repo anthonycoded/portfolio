33e68cc52bdcf6d071f245a67a1a977149683fcc
anthonycoded <barbels33@gmail.com>
2021-04-04 13:36:10 -0400

    technologies v2

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -32,6 +32,9 @@ import mui from "../assets/material-ui.svg";
 import bootstrap from "../assets/Bootstrap_logo.svg";
 import webpackcube from "../assets/webpackcube.svg";
 import devices from "../assets/devices.svg";
+import gcloud from "../assets/gcloud.png";
+import nodejs from "../assets/nodejs.svg";
+import mongodb from "../assets/mongodb.png"
 
 const LandingPage = () => {
   return (
@@ -75,30 +78,30 @@ const LandingPage = () => {
           d="M0,128L48,144C96,160,192,192,288,176C384,160,480,96,576,69.3C672,43,768,53,864,90.7C960,128,1056,192,1152,224C1248,256,1344,256,1392,256L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
         ></path>
       </svg>
-      <div className="h-full w-full bg-material-purple flex flex-col items-center p-8">
+      <div className="h-full w-full bg-material-purple flex flex-col items-center p-8 space-y-8">
         <p className="text-2xl text-left w-full mb-4 font-bold">
           What I do is simple
         </p>
-        <p className="text-white font-medium text-xl mb-8">
+        <p className="text-white font-medium text-xl mb-20">
           I Create online business solutions that empowers a company to not only
           grow but also thrive in todays competitve world.
         </p>
-        <div className="flex flex-col w-full space-y-12 p-4">
-          <div className="flex w-full justify-start items-center space-x-4">
+        <div className="flex flex-col w-full h-full space-y-16 p-4 mt-12">
+          <div className="flex w-full h-full justify-start items-center space-x-4">
             <p className="text-white text-xl font-bold w-32">
               Create Custom Solutions
             </p>
             <img src={cube} alt="" className="h-24"></img>
           </div>
           <img src={down} alt="" className="h-12"></img>
-          <div className="flex items-center justify-end w-full space-x-4">
+          <div className="flex items-center justify-end w-full h-full space-x-4">
             <img src={business} alt="" className="h-24"></img>
             <p className="text-white text-xl font-bold w-32">
               Empower your business to grow
             </p>
           </div>
           <img src={down} alt="" className="h-12"></img>
-          <div className="flex justify-start items-center space-x-4 w-full">
+          <div className="flex justify-start items-center space-x-4 w-full h-full">
             <p className="text-white text-xl font-bold w-32">
               Compete and succeed!
             </p>
@@ -221,30 +224,25 @@ const LandingPage = () => {
           backgroundSize: "cover",
         }}
       >
-        <div className="p-8">
+        <div className="py-12">
           <p className="text-center text-material-blue mb-12 text-2xl font-bold">
             Technologies
           </p>
-          <div className="flex flex-col h-full w-full gap-8">
-            <div className="flex justify-between items-center space-x-8 w-full h-full mb-8">
-              <div className="flex items-center w-72 justify-between">
-                <div className="flex flex-col items-center justify-center h-full">
-                  <img src={html} alt="" className="h-16"></img>
-                  <p className="text-base text-white font-medium">HTML5</p>
-                </div>
-                <div className="flex flex-col justify-center items-center h-full">
-                  <img
-                    src={css}
-                    alt=""
-                    className="h-16 w-to bg-white rounded-full"
-                  ></img>
-                  <p className="text-base text-white font-medium">CSS</p>
-                </div>
-              </div>
-              <div className="w-full flex flex-col items-end h-full space-y-4">
+          <div className="flex flex-col h-full w-full">
+            <div className="flex justify-center items-center w-full h-full mb-8 space-x-7">
+              <img src={html} alt="" className="h-16 bg-white rounded-xl"></img>
+
+              <img src={css} alt="" className="h-16  bg-white rounded-xl"></img>
+              <img
+                src={down}
+                alt=""
+                className="h-12 transform -rotate-90"
+              ></img>
+
+              <div className="flex flex-col items-center h-full space-y-4">
                 <div className="flex items-center space-x-2">
                   <img src={bootstrap} alt="" className="h-7 "></img>
-                  <p className="">Bootstrap</p>
+                  <p className="text-white font-bold">Bootstrap</p>
                 </div>
                 <div className="flex items-center space-x-2">
                   <img
@@ -252,7 +250,7 @@ const LandingPage = () => {
                     alt=""
                     className="h-7 w-7 rounded-full bg-white"
                   ></img>
-                  <p className="">Material-UI</p>
+                  <p className="text-white font-bold">Material-UI</p>
                 </div>
                 <div className="flex items-center space-x-2">
                   <img
@@ -260,10 +258,11 @@ const LandingPage = () => {
                     alt=""
                     className="h-7 rounded-full bg-white"
                   ></img>
-                  <p className="TailwindCSS">Tailwind</p>
+                  <p className="text-white font-bold">Tailwind</p>
                 </div>
               </div>
             </div>
+            <img src={down} alt="" className="h-12"></img>
 
             <div className="flex items-center p-4 justify-between">
               <div className="flex flex-col items-center justify-center w-full h-full">
@@ -271,19 +270,30 @@ const LandingPage = () => {
                 <p className="text-base text-white font-medium">Javascript</p>
               </div>
               <div className="items-end w-full grid grid-cols-2 justify-items-center content-center pl-16">
-                <p className="text-sm text-white h-12">Vanilla</p>
-                <p className="text-sm text-white h-12">React</p>
-                <p className="text-sm text-white h-12">ESNext</p>
-                <p className="text-sm text-white h-12">AJAX</p>
-                <p className="text-sm text-white h-12">JSON</p>
-                <p className="text-sm text-white h-12">DOM</p>
+                <p className="text-sm text-white h-12 font-bold">Vanilla</p>
+                <p className="text-sm text-white h-12 font-bold">React</p>
+                <p className="text-sm text-white h-12 font-bold">ESNext</p>
+                <p className="text-sm text-white h-12 font-bold">AJAX</p>
+                <p className="text-sm text-white h-12 font-bold">JSON</p>
+                <p className="text-sm text-white h-12 font-bold">DOM</p>
               </div>
             </div>
+            <img src={down} alt="" className="h-12 mb-8"></img>
 
             <div className="flex flex-col items-center">
-              <img src={webpackcube} alt="" className="h-20"></img>
+              <img
+                src={webpackcube}
+                alt=""
+                className="h-20 animate-spin-slow duration-400"
+              ></img>
               <p className="text-xl font-bold text-white">Webpack</p>
             </div>
+          </div>
+          <div className="w-full h-full p-4 space-y-8">
+            <p className="text-2xl font-bold text-white text-center w-full">Backend</p>
+            <img src={nodejs} alt="mongodb" className="h-24"></img>
+            <img src={mongodb} alt="mongodb" className="h-9 bg-white rounded"></img>
+            <img src={gcloud} alt="mongodb" className="h-24"></img>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "material-purple": "#aa00ff",
-        "material-blue": "#00e5ff"
+        "material-blue": "#00e5ff",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
       },
     },
   },
