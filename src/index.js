462bfff2d2827cea03c1a8681746ed136f3f0d22
anthonycoded <barbels33@gmail.com>
2021-04-08 21:48:44 -0400

    added firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
 
