bda41f16960d81bb8e22989c051c15141b3ae225
anthonycoded <barbels33@gmail.com>
2021-04-08 21:55:05 -0400

    meta data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 // must be listed before other Firebase SDKs
 import firebase from "firebase/app";
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
-
+ReactDOM.render(<App></App>, document.getElementById("root"));
