934068447f1728dd9f4a1f3505b0bc81e3787bf1
anthonycoded <barbels33@gmail.com>
2022-08-14 23:30:47 -0400

    updated to new react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// must be listed before other Firebase SDKs
-import firebase from "firebase/app";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //
+  // <BrowserRouter basename="/milly/">
+  //
+  <BrowserRouter>
+    <App></App>
+  </BrowserRouter>
+);
