22f80a3843e2bac74467a1287d42a8a8c2f9fca6
anthonycoded <barbels33@gmail.com>
2022-08-14 23:31:24 -0400

    relocated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import menu from "../../assets/menu.svg";
+import menu from "../assets/menu.svg";
 import Drawer from "./Drawer";
 
 const Header = () => {
