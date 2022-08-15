a8135793e6450e11eac558b41b602396edc05192
anthonycoded <barbels33@gmail.com>
2022-08-14 23:30:31 -0400

    createed appb wrapper

---
+++ src/AppWrapper.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./pages/landing/LandingPage";
+
+//import ScrollToTop from "./utils/utils";
+
+const AppWrapper = () => {
+  return (
+    <>
+      <Header className="display-block"></Header>
+
+      <div className="pt-12">
+        <Routes>
+          <Route path="/">
+            <Route index element={<Homepage />}></Route>
+          </Route>
+        </Routes>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default AppWrapper;
