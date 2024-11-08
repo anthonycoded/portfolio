7a450d401a8e85736a2101bfd3c1f730e820529e
Shawn <barbels33@gmail.com>
2024-11-08 03:07:24 -0500

    moved and refactored

---
+++ src/components/nav/Drawer.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import routes from "./routes";
+
+type Props = {
+  setOpen: (arg0: boolean) => void;
+};
+
+const Drawer = ({ setOpen }: Props) => {
+  const closeDrawer = () => {
+    setOpen(false);
+  };
+  // useEffect(() => {
+  //   document.body.addEventListener("click", closeDrawer);
+  //   return function cleanup() {
+  //     document.body.removeEventListener("click", closeDrawer);
+  //   };
+  // }, []);
+
+  return (
+    <div
+      className="bg-material-blue  w-72 top-12 right-0 transform flex flex-col fixed rounded-l-xl"
+      //onMouseLeave={(e) => setOpen(false)}
+    >
+      <div className="flex flex-col p-8 space-y-4">
+        {routes.map((route, i) => (
+          <Link
+            key={i}
+            to={route.route}
+            className="text-2xl font-medium h-8 flex items-center"
+            onClick={closeDrawer}
+          >
+            {route.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
---
+++ src/components/nav/Header.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Drawer from "./Drawer";
+import routes from "./routes";
+import menu from "../../assets/menu.svg";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(open);
+  }, [open]);
+
+  return (
+    <div className="w-full h-12 fixed   z-10">
+      <div className="h-12 flex justify-between items-center bg-material-purple w-full px-2 fixed top-0 shadow-2xl">
+        <a href="/">
+          <h1 className="pl-4 text-material-blue font-bold text-2xl">
+            <p className="text-black">
+              <span className="text-material-blue">Shawn</span>Barbel
+            </p>
+          </h1>
+        </a>
+
+        <div className="md:flex hidden space-x-4 px-4">
+          {routes.map((route, i) => (
+            <Link
+              key={i}
+              to={route.route}
+              className="text-xl font-medium h-8 flex items-center"
+              onClick={() => setOpen(false)}
+            >
+              {route.title}
+            </Link>
+          ))}
+        </div>
+
+        <button className="md:hidden" onClick={() => setOpen(!open)}>
+          <img src={menu} alt="menu" className="h-9"></img>
+        </button>
+      </div>
+      {open ? <Drawer setOpen={setOpen}></Drawer> : undefined}
+    </div>
+  );
+};
+
+export default Header;
