664093a049a838d8742b45ea7f8c3eaaaf5d744d
Shawn <barbels33@gmail.com>
2024-12-12 07:10:19 -0500

    created data of projects

---
+++ src/pages/data.ts
@@ -0,0 +1,202 @@
+import giftlist2 from '../assets/giftlist2.png';
+
+export const projects = [
+  {
+    name: 'Giftlist',
+    title: 'Personal Wishlist',
+    tags: ['E-Commerce', 'Content Management'],
+    description:
+      "Are you looking for the perfect holiday gift for Shawn? You're in the right place! Shawn's Giftlist makes it easy to browse a curated list of items from popular retailers like Walmart, Amazon, and Best Buy.",
+    images: [giftlist2],
+    link: 'https://giftlist-lovat.vercel.app/',
+    stack: [
+      'NextJS',
+      'TypeScript',
+      'TailwindCSS',
+      'Vercel',
+      'Django',
+      'PostgreSQL',
+      'Google Cloud Platform',
+    ],
+    stackType: 'Full Stack',
+    technologies: [
+      { name: 'Sentry Reporting', type: 'Error Reporting' },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Web'],
+  },
+  {
+    name: 'Uber Clone',
+    title: 'Uber Clone',
+    tags: ['UI/UX', 'Figma'],
+    description:
+      'This is my version of an Uber app. The purpoose of this is to showcase some of my UI/UX skills.',
+    images: [],
+    link: 'https://giftlist-lovat.vercel.app/',
+    stack: ['React Native', 'Expo', 'Typescript'],
+    stackType: 'Frontend',
+    technologies: [{ name: 'Github', type: 'Version Control' }],
+    platforms: ['IOS/Android'],
+  },
+
+  {
+    name: 'scraper',
+    title: 'Python AI Web Scraper',
+    tags: ['LLM', 'AI', 'Scraper'],
+    description:
+      'With this ai powered web scraper you can scrape a website and ask the ai anything about the parsed information.',
+    images: [],
+    link: '',
+    stack: ['Python'],
+    stackType: 'Full Stack',
+    technologies: [
+      {
+        name: 'Ollama',
+        type: 'LLM',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Web'],
+  },
+  {
+    name: 'Youtube Video Downloader',
+    title: 'Youtube Video Downloader',
+    tags: ['Youtube', 'Videos'],
+    description:
+      "Are you looking for the perfect holiday gift for Shawn? You're in the right place! Shawn's Giftlist makes it easy to browse a curated list of items from popular retailers like Walmart, Amazon, and Best Buy.",
+    images: [],
+    link: '',
+    stack: ['Python'],
+    stackType: 'Full Stack',
+    platforms: ['Web'],
+    technologies: [{ name: 'Github', type: 'Version Control' }],
+  },
+  {
+    name: 'Zaneri',
+    title: 'Portfolio Chatbot',
+    tags: ['Chatbot', 'AI', 'LLM', 'Ollama'],
+    description:
+      "Are you looking for the perfect holiday gift for Shawn? You're in the right place! Shawn's Giftlist makes it easy to browse a curated list of items from popular retailers like Walmart, Amazon, and Best Buy.",
+    images: [],
+    link: '',
+    stack: ['React', 'Typescript', 'TailwindCSS', 'Python', 'Google Cloud'],
+    stackType: 'Full Stack',
+    technologies: [
+      {
+        name: 'Ollama',
+        type: 'LLM',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Web'],
+  },
+  {
+    name: 'MatoMan',
+    title: 'Crypto Trading Bot',
+    tags: ['Crypto', 'Web3', 'Solana', 'Automation'],
+    description:
+      "Are you looking for the perfect holiday gift for Shawn? You're in the right place! Shawn's Giftlist makes it easy to browse a curated list of items from popular retailers like Walmart, Amazon, and Best Buy.",
+    images: [],
+    link: '',
+    stack: ['Telegram', 'NodeJS'],
+    stackType: 'Full Stack',
+    technologies: [
+      {
+        name: 'Web3',
+        type: 'Protocol',
+      },
+      {
+        name: 'Solana',
+        type: 'Blockchain',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Telegram Bot'],
+  },
+  {
+    name: 'Stocked Up',
+    title: 'Stock Trading Bot',
+    tags: ['Stocks', 'Trading', 'Automation', 'Fintech'],
+    description: '',
+    images: [],
+    link: '',
+    stack: ['Telegram', 'NodeJS'],
+    stackType: 'Full Stack',
+    technologies: [
+      {
+        name: 'Web3',
+        type: 'Protocol',
+      },
+      {
+        name: 'Alpaca',
+        type: '3rd Party API',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Telegram Bot'],
+  },
+  {
+    name: "Who's who",
+    title: 'Facial Recognition',
+    tags: ['AI'],
+    description: '',
+    images: [],
+    link: '',
+    stack: ['Python', 'Tensorflow'],
+    stackType: 'Backend',
+    technologies: [{ name: 'Github', type: 'Version Control' }],
+    platforms: [],
+  },
+  {
+    name: 'Imaigine',
+    title: 'AI Image Generator',
+    tags: ['AI'],
+    description: '',
+    images: [],
+    link: '',
+    stack: ['Python', 'NodeJS'],
+    stackType: 'Backend',
+    technologies: [
+      {
+        name: 'Web3',
+        type: 'Protocol',
+      },
+      {
+        name: 'Solana',
+        type: 'Blockchain',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Web', 'ios', 'Android'],
+  },
+  {
+    name: 'Green Thumbs',
+    title: 'Garden Planner',
+    tags: ['Agriculture'],
+    description: '',
+    images: [],
+    link: '',
+    stack: [
+      'NextJS',
+      'Typescript',
+      'TailwindCSS',
+      'python',
+      'Django',
+      'Postgres',
+      'Google Cloud',
+    ],
+    stackType: 'Backend',
+    technologies: [
+      {
+        name: 'Web3',
+        type: 'Protocol',
+      },
+      {
+        name: 'Solana',
+        type: 'Blockchain',
+      },
+      { name: 'Github', type: 'Version Control' },
+    ],
+    platforms: ['Web', 'ios', 'Android'],
+  },
+];
